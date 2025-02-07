37cba97f18a3c902eacdd9b9ef8cd4e6c4649979
Shreya Sanghamitra <sanghapraju@gmail.com>
2025-02-07 11:50:59 -0500

    Your commit message here

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
-import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import LoginPanel from "./components/Login/Login";  // Import the Login component
+import Register from "./components/Register/Register";  // Import the Register component
+import { Routes, Route } from "react-router-dom";  // Import necessary components for routing
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPanel />} />
+      <Route path="/login" element={<LoginPanel />} />  {/* This is the Login page route */}
+      <Route path="/register" element={<Register />} />  {/* This is the Register page route */}
     </Routes>
   );
 }
+
 export default App;
